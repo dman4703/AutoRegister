80efd5f3205962db3b1fe8af083182181770aa72
Devon OQuinn <douqinn31@gmail.com>
2024-07-16 20:26:56 -0400

    script can now succesfully check course availability and register as needed. also has sms capability

---
+++ Driver.py
@@ -0,0 +1,13 @@
+import Register
+
+Register.login()
+Register.selectTerm()
+# Register.findCourse('CSCI', '4300')
+'''if checkCourseAvailability(44457):
+    unregister(59894)
+    register(44457)
+elif checkCourseAvailability(31786):
+    unregister(59894)
+    register(31786)
+else:
+    print("No seats available")'''
--- Register.py
+++ Register.py
@@ -1,109 +1,85 @@
+import time
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import SMS
+import os
 
 driver = webdriver.Chrome()
-
+wait = WebDriverWait(driver, 10)
 
 def login():
     driver.get('https://sso.uga.edu/cas/login?TARGET=https%3A%2F%2Fathena-prod.uga.edu%2FStudentSelfService%2Flogin%2Fcas')
     username = driver.find_element(By.ID, 'username')
     password = driver.find_element(By.ID, 'password')
-    username.send_keys('dko48341')
-    password.send_keys('Dd20040929+@')
+    username.send_keys(os.environ.get('userID'))
+    password.send_keys(os.environ.get('password'))
     driver.find_element('name', 'submit').click()
+    SMS.send(os.environ.get('phone'), 'tmobile', wait.until(EC.presence_of_element_located((By.CLASS_NAME, "verification-code"))).text)
+    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, "dont-trust-browser-button"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//label[text()='Student & Registration']"))).click()
+    driver.find_element(By.XPATH, '//a[@href="https://sso.uga.edu/cas/login?TARGET=https%3A%2F%2Fathena-prod.uga.edu%2FStudentRegistrationSsb%2Flogin%2Fcas"]').click()
+    driver.switch_to.window(driver.window_handles[-1])
+    wait.until(EC.element_to_be_clickable((By.ID, "registerLink"))).click()
 
 
 def selectTerm():
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "toggle_1"))).click()
-    driver.find_element(By.XPATH, '//a[@href="https://sso.uga.edu/cas/login?TARGET=https%3A%2F%2Fathena-prod.uga.edu%2FStudentRegistrationSsb%2Flogin%2Fcas"]').click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "registerLink"))).click()
-    driver.get('https://athena-prod.uga.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=registration')
     dropdown = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "txt_term")))
+        EC.element_to_be_clickable((By.ID, "s2id_txt_term")))
     dropdown.click()
-    dropdown.send_keys("Fall 2024")
-    dropdown.send_keys(Keys.RETURN)
+    wait.until(EC.element_to_be_clickable((By.ID, "202408"))).click()
     driver.find_element(By.ID, 'term-go').click()
 
 
-def findCourse(S, C):
-    subject = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "s2id_autogen8")))
-    subject.click()
-    subject.send_keys(S)
-    subject.send_keys(Keys.RETURN)
-    courseNumber = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "txt_courseNumber")))
-    courseNumber.click()
-    courseNumber.send_keys(C)
-    courseNumber.send_keys(Keys.RETURN)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "search-go"))).click()
-
+def findCourse(crn):
+    keyword = wait.until(EC.element_to_be_clickable((By.ID, "txt_keywordlike")))
+    keyword.send_keys(crn)
+    keyword.send_keys(Keys.RETURN)
 
 def checkCourseAvailability(crn):
-    rows = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#searchResultsTable tr[data-id]")))
-    for row in rows:
-        courseCRN = row.find_element(By.XPATH, './/td[@data-property="courseReferenceNumber"]').text
-        if courseCRN == str(crn):
-            detailsLink = row.find_element(By.CSS_SELECTOR, ".section-details-link")
-            detailsLink.click()
-
-            # Wait for the details to load and check the enrollment seats available
-            enrollmentSeats = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, "//span[contains(text(), 'Enrollment Seats Available:')]/following-sibling::span"))
-            ).text
-
-            # Convert the text to an integer to check if seats are available
-            seatsAvailable = int(enrollmentSeats)
-            result = seatsAvailable > 0
-
-            # Close the popup
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ui-icon.ui-icon-closethick"))).click()
-
-            return result
-    return False
+    driver.get('https://athena-prod.uga.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=registration')
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//span[@class='title' and text()='Browse Classes']"))).click()
+    selectTerm()
+    findCourse(crn)
+    row = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#searchResultsTable tr[data-id]")))
+    status = row.find_element(By.XPATH, './/td[@data-property="status"]').text
+    while 'FULL' in status.upper():
+        time.sleep(10)
+        wait.until(EC.element_to_be_clickable((By.ID, 'search-again-button'))).click()
+        driver.find_element(By.ID, 'search-go').click()
+        row = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#searchResultsTable tr[data-id]")))
+        status = row.find_element(By.XPATH, './/td[@data-property="status"]').text
+    return True
 
 
 def register(crn):
-    rows = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#searchResultsTable tr[data-id]")))
-    for row in rows:
-        courseCrn = row.find_element(By.XPATH, './/td[@data-property="courseReferenceNumber"]').text
-        if courseCrn == str(crn):
-            row.find_element(By.ID, f'addSection202408{crn}').click()
-
-            # Click the submit button to register
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'saveButton'))).click()
+    row = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#searchResultsTable tr[data-id]")))
+    row.find_element(By.ID, f'addSection202408{crn}').click()
+    wait.until(EC.element_to_be_clickable((By.ID, 'saveButton'))).click()
+    time.sleep(10)
 
 
 def unregister(crn):
-    rows = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((
+    rows = wait.until(EC.presence_of_all_elements_located((
             By.CSS_SELECTOR, ".ui-layout-east.ui-widget-content.mySchedule.mySchedule-summary tr[data-id]")))
     for row in rows:
         courseCrn = row.find_element(By.XPATH, './/td[@data-property="courseReferenceNumber"]').text
         if courseCrn == str(crn):
-            row.find_element(By.ID, f'action-{crn}-ddl').click()
+            row.find_element(By.XPATH, f'.//div[@id="s2id_action-{crn}-ddl"]').click()
+            from selenium.webdriver.common.action_chains import ActionChains
+            actions = ActionChains(driver)
+            actions.send_keys(Keys.ARROW_DOWN)
+            actions.send_keys(Keys.ENTER)
+            actions.perform()
+            break
 
-            # Select "Web Drop" from the dropdown
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f"//li[@role='option']//div[text()='Web Drop']"))
-            ).click()
-            return True
-    return False
 
 
-login()
-selectTerm()
-''' findCourse('CSCI', '4300')
-if checkCourseAvailability(44457):
+if(checkCourseAvailability(44457)):
+    login()
+    selectTerm()
+    findCourse(44457)
     unregister(59894)
-    register(44457)
-elif checkCourseAvailability(31786):
-    unregister(59894)
-    register(31786)
-else:
-    print("No seats available")'''
+    register()
